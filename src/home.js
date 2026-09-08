2e1c45755a264f88020d03d8f7ba521c5f0d1cb1
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-09-08 12:14:14 +0530

    updated new banners

--- src/home.js
+++ src/home.js
@@ -185,12 +185,12 @@ const InterestForm = () => {
       </nav>
 
       <img
-        src='/images/banners/Website_Hyundai_August_Independence_offers_Bharat_Hyundai_Offers.webp'
+        src='/new-banners/Website_Hyundai_August_Independence_offers_Bharat_Hyundai_Offers.webp'
         alt='Banner'
         className='hidden object-cover w-full mt-12 sm:block'
       />
       <img
-        src='/images/banners/WM_Hyundai_August_Independence_offers_Bharat_Hyundai_Offers.webp'
+        src='/new-banners/WM_Hyundai_August_Independence_offers_Bharat_Hyundai_Offers.webp'
         alt='Mobile Banner'
         className='block object-cover w-full mt-10 sm:hidden'
       />
